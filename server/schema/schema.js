5266361780a2869f462b45422033d2ef8529c462
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-10 14:27:54 -0400

    m

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
-    fields: () => ({
+    fields: ( ) => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
@@ -14,18 +22,17 @@ const BookType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        book: { 
+        book: {
             type: BookType,
             args: { id: { type: GraphQLString } },
             resolve(parent, args){
-                args.id
-                //code to get data from db/other source
-
+                // code to get data from db / other source
+                return _.find(books, { id: args.id });
             }
-         }
+        }
     }
 });
 
 module.exports = new GraphQLSchema({
     query: RootQuery
-})
+});
