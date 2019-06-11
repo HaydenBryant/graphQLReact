3345cebe5dd32c7fe0665a8c57b84ffcdb598350
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-11 13:46:02 -0400

    added mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to atlas
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log("mongo db has connected")
+    )
+    .catch(err => console.log(err));
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
