046136029f104ee8740c07e24a7ce80b9e97d5f1
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-11 23:06:25 -0400

    lesson 28. Connected to monogo, begining react

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//cross origin requests allowed
+app.use(cors());
 
 // connect to atlas
 const db = require("./config/keys").mongoURI;
